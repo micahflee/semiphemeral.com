4c7c752edc3be1a9c8208836bfcabe5784e70105
Micah Lee <micah@micahflee.com>
2019-12-26 23:47:42 -0800

    Starting to make authenticating with twitter work

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,30 +1,98 @@
 #!/usr/bin/env python3
 import os
+import base64
 import tweepy
 import asyncio
+import logging
+from cryptography import fernet
 from aiohttp import web
+from aiohttp_session import setup, get_session
+from aiohttp_session.cookie_storage import EncryptedCookieStorage
 
 
-async def handle(request):
-    name = request.match_info.get("name", "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+async def login(request):
+    session = await get_session(request)
+    if "api" in session:
+        # If we're already logged in, redirect
+        try:
+            session["api"].me()
+            raise web.HTTPFound(
+                location=f"https://{os.environ.get('FRONTEND_DOMAIN')}/app"
+            )
+        except:
+            # Not actually logged in
+            pass
+
+    # Otherwise, authorize with Twitter
+    try:
+        auth = tweepy.OAuthHandler(
+            os.environ.get("TWITTER_CONSUMER_TOKEN"),
+            os.environ.get("TWITTER_CONSUMER_KEY"),
+        )
+        redirect_url = auth.get_authorization_url()
+        session["oauth_token"] = auth.request_token["oauth_token"]
+        raise web.HTTPFound(location=redirect_url)
+    except tweepy.TweepError:
+        raise web.HTTPUnauthorized(
+            text="Error, failed to get request token from Twitter"
+        )
 
 
-def main():
-    # Initialize tweepy
-    auth = tweepy.OAuthHandler(
+async def twitter_auth(request):
+    params = request.rel_url.query
+    if "oauth_token" not in params or "oauth_verifier" not in params:
+        raise web.HTTPUnauthorized(
+            text="Error, oauth_token and oauth_verifier are required"
+        )
+
+    session = await get_session(request)
+    oauth_token = session["oauth_token"]
+    verifier = params["oauth_verifier"]
+    del session["oauth_token"]
+
+    # Authenticate with twitter
+    session["auth"] = tweepy.OAuthHandler(
         os.environ.get("TWITTER_CONSUMER_TOKEN"),
         os.environ.get("TWITTER_CONSUMER_KEY"),
     )
-    auth.set_access_token(
-        os.environ.get("TWITTER_ACCESS_TOKEN"), os.environ.get("TWITTER_ACCESS_KEY")
-    )
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    session["auth"].request_token = {
+        "oauth_token": oauth_token,
+        "oauth_token_secret": verifier,
+    }
 
-    # Start the web app
+    try:
+        session["auth"].get_access_token(verifier)
+    except tweepy.TweepError:
+        raise web.HTTPUnauthorized(text="Error, failed to get access token")
+
+    session["api"] = tweepy.API(
+        session["auth"], wait_on_rate_limit=True, wait_on_rate_limit_notify=True
+    )
+
+    # Redirect to app
+    raise web.HTTPFound(location=f"https://{os.environ.get('FRONTEND_DOMAIN')}/app")
+
+
+def main():
     app = web.Application()
-    app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
+    logging.basicConfig(level=logging.DEBUG)
+
+    # Enable the debug toolbar in staging
+    if os.environ.get("DEPLOY_ENVIRONMENT") == "staging":
+        import aiohttp_debugtoolbar
+        from aiohttp_debugtoolbar import toolbar_middleware_factory
+
+        aiohttp_debugtoolbar.setup(app)
+
+    # secret_key must be 32 url-safe base64-encoded bytes
+    fernet_key = fernet.Fernet.generate_key()
+    secret_key = base64.urlsafe_b64decode(fernet_key)
+    setup(app, EncryptedCookieStorage(secret_key))
+
+    # Define routes
+    app.add_routes(
+        [web.get("/login", login), web.get("/twitter_auth", twitter_auth),]
+    )
     web.run_app(app)
 
 
--- app/frontend/pages/index.js
+++ app/frontend/pages/index.js
@@ -1,7 +1,15 @@
-export default function Index() {
+function Index() {
     return (
         <div>
-            <p>Hello world</p>
+            <img src="/img/logo.png" alt="Semiphemeral" />
+            <p><a href="https://api.staging.semiphemeral.com/login">Sign in with Twitter</a></p>
+            <style jsx>{`
+                div {
+                    text-align: center;
+                }
+            `}</style>
         </div>
     )
 }
+
+export default Index
