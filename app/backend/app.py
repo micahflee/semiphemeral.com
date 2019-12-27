f204831ba7a751a81122418c49f5d75c50cc694f
Micah Lee <micah@micahflee.com>
2019-12-26 21:22:28 -0800

    Add an aiohttp-based backend, and make it deploy its own container, and configure nginx to work with it

---
+++ app/backend/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from aiohttp import web
+
+
+async def handle(request):
+    name = request.match_info.get("name", "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+
+app = web.Application()
+app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
+
+if __name__ == "__main__":
+    web.run_app(app)
