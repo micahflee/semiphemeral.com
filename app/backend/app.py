99bb0d8a69ff249d829fb347ab2e8354371c1999
Micah Lee <micah@micahflee.com>
2019-12-26 22:21:05 -0800

    Split vars into separate files for terraform and ansible vars, and pass Twitter creds through ansible to the backend container, so tweepy can use them to authenticate to Twitter

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+import os
+import tweepy
+import asyncio
 from aiohttp import web
 
 
@@ -9,8 +11,22 @@ async def handle(request):
     return web.Response(text=text)
 
 
-app = web.Application()
-app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
+def main():
+    # Initialize tweepy
+    auth = tweepy.OAuthHandler(
+        os.environ.get("TWITTER_CONSUMER_TOKEN"),
+        os.environ.get("TWITTER_CONSUMER_KEY"),
+    )
+    auth.set_access_token(
+        os.environ.get("TWITTER_ACCESS_TOKEN"), os.environ.get("TWITTER_ACCESS_KEY")
+    )
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+    # Start the web app
+    app = web.Application()
+    app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
+    web.run_app(app)
+
 
 if __name__ == "__main__":
-    web.run_app(app)
+    main()
