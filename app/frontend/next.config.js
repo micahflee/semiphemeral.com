a2037226a4389b770700917a0393b170a5c64baf
Micah Lee <micah@micahflee.com>
2019-12-28 15:42:02 -0800

    Pass args into frontend docker container during build, in order for publicRuntimeConfig to work while containerized

--- app/frontend/next.config.js
+++ app/frontend/next.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    serverRuntimeConfig: {
-    },
     publicRuntimeConfig: {
         frontendDomain: process.env.FRONTEND_DOMAIN,
         backendDomain: process.env.BACKEND_DOMAIN
