f3a1b21c2abb8611f9fb051f063618266bf00452
Micah Lee <micah@micahflee.com>
2019-12-28 14:34:04 -0800

    Add a digitalocean firewall: you can only ssh from devops IP, and on staging you can only connect to web from devops IP, except during deploy when all IPs are allowed for LE

--- terraform/modules/semiphemeral/main.tf
+++ terraform/modules/semiphemeral/main.tf
@@ -1,5 +1,7 @@
 variable "deploy_environment" {}
 variable "ssh_fingerprint" {}
+variable "ssh_ips" {}
+variable "inbound_ips" {}
 
 resource "digitalocean_droplet" "app" {
   name               = "app-${var.deploy_environment}"
@@ -12,6 +14,50 @@ resource "digitalocean_droplet" "app" {
   ]
 }
 
+resource "digitalocean_firewall" "app" {
+  name        = "only-22-80-and-443"
+  droplet_ids = [digitalocean_droplet.app.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = jsondecode(var.ssh_ips)
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = jsondecode(var.inbound_ips)
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = jsondecode(var.inbound_ips)
+  }
+
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = jsondecode(var.inbound_ips)
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "53"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
 
 output "app_ip" {
   value = digitalocean_droplet.app.ipv4_address
--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -1,6 +1,8 @@
 variable "deploy_environment" {}
 variable "ssh_fingerprint" {}
 variable "do_token" {}
+variable "ssh_ips" {}
+variable "inbound_ips" {}
 
 provider "digitalocean" {
   token = var.do_token
@@ -10,6 +12,8 @@ module "semiphemeral" {
   source             = "../modules/semiphemeral"
   deploy_environment = var.deploy_environment
   ssh_fingerprint    = var.ssh_fingerprint
+  ssh_ips            = var.ssh_ips
+  inbound_ips        = var.inbound_ips
 }
 
 output "app_ip" {
