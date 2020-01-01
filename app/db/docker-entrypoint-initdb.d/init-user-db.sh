ea23a2d53a29c2f7dd9f0fe3cdd8fb8cce2e1f53
Micah Lee <micah@micahflee.com>
2020-01-01 15:31:43 -0800

    Fix db to just use postgres image directly

--- app/db/docker-entrypoint-initdb.d/init-user-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE USER semiphemeral;
-    CREATE DATABASE semiphemeral;
-    GRANT ALL PRIVILEGES ON DATABASE semiphemeral TO semiphemeral;
-EOSQL
