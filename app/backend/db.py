aef76df4cb25b627b4e7914d21af47a2005b8e3e
Micah Lee <micah@micahflee.com>
2020-01-05 13:13:21 -0800

    Start implementing gino

---
+++ app/backend/db.py
@@ -0,0 +1,21 @@
+import os
+from gino import Gino
+
+db = Gino()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer(), primary_key=True)
+    twitter_id = db.Column(db.Integer())
+    twitter_screen_name = db.Column(db.String())
+    twitter_access_token = db.Column(db.String())
+    twitter_access_token_secret = db.Column(db.String())
+
+
+async def connect():
+    password = os.environ.get("POSTGRES_PASSWORD")
+    await db.set_bind(f"postgresql://semiphemeral:{password}@semiphemeral/semiphemeral")
+    await db.gino.create_all()
+    return db
