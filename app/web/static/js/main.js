2d753ebbbb656d05abefb3b272abac8bea3b757f
Micah Lee <micah@micahflee.com>
2020-01-19 11:15:09 -0800

    Start making an app-nav component

--- app/web/static/js/main.js
+++ app/web/static/js/main.js
@@ -1,4 +1,19 @@
-var app = new VTTCue({
+Vue.component('app-nav', {
+    template: `
+        <div class="nav">
+            <span class="logo"><a href="/"><img src="/static/img/logo-small.png" /></a></span>
+            <ul>
+                <li>Dashboard</li>
+                <li>Tweets</li>
+                <li>Settings</li>
+                <li>Tip</li>
+            </ul>
+            <span class="user">@twitter_user</span>
+        </div>
+    `
+})
+
+var app = new Vue({
     el: "#app",
     data: {}
 })
