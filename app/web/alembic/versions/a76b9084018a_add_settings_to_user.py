42b5711ca2334f02a33ca68b92f1bafd038dac3c
Micah Lee <micah@micahflee.com>
2020-01-19 16:00:34 -0800

    Start adding settings to the User model

---
+++ app/web/alembic/versions/a76b9084018a_add_settings_to_user.py
@@ -0,0 +1,81 @@
+"""add settings to user
+
+Revision ID: a76b9084018a
+Revises: 6b7f5885b1cf
+Create Date: 2020-01-19 15:51:33.148531
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "a76b9084018a"
+down_revision = "6b7f5885b1cf"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "users", sa.Column("delete_tweets", sa.Boolean, default=False, nullable=False)
+    )
+    op.add_column(
+        "users",
+        sa.Column("tweets_days_threshold", sa.Integer, default=30, nullable=False),
+    )
+    op.add_column(
+        "users",
+        sa.Column("tweets_retweet_threshold", sa.Integer, default=20, nullable=False),
+    )
+    op.add_column(
+        "users",
+        sa.Column("tweets_like_threshold", sa.Integer, default=20, nullable=False),
+    )
+    op.add_column(
+        "users", sa.Column("retweets_likes", sa.Boolean, default=False, nullable=False)
+    )
+    op.add_column(
+        "users",
+        sa.Column(
+            "retweets_likes_delete_retweets", sa.Boolean, default=True, nullable=True
+        ),
+    )
+    op.add_column(
+        "users",
+        sa.Column(
+            "retweets_likes_retweets_threshold", sa.Integer, default=30, nullable=False
+        ),
+    )
+    op.add_column(
+        "users",
+        sa.Column(
+            "retweets_likes_delete_likes", sa.Boolean, default=True, nullable=True
+        ),
+    )
+    op.add_column(
+        "users",
+        sa.Column(
+            "retweets_likes_likes_threshold", sa.Integer, default=60, nullable=False
+        ),
+    )
+    op.add_column(
+        "users", sa.Column("since_id", sa.String(), nullable=False),
+    )
+    op.add_column(
+        "users", sa.Column("last_fetch", sa.DateTime(), nullable=True),
+    )
+
+
+def downgrade():
+    op.drop_column("users", "delete_tweets")
+    op.drop_column("users", "tweets_days_threshold")
+    op.drop_column("users", "tweets_retweet_threshold")
+    op.drop_column("users", "tweets_like_threshold")
+    op.drop_column("users", "retweets_likes")
+    op.drop_column("users", "retweets_likes_delete_retweets")
+    op.drop_column("users", "retweets_likes_retweets_threshold")
+    op.drop_column("users", "retweets_likes_delete_likes")
+    op.drop_column("users", "retweets_likes_likes_threshold")
+    op.drop_column("users", "since_id")
+    op.drop_column("users", "last_fetch")
--- app/web/static/js/components/settings.js
+++ app/web/static/js/components/settings.js
@@ -2,6 +2,72 @@ Vue.component('settings', {
     template: `
         <div class="page settings">
             <h1>Settings</h1>
+            <form method="post" action="/settings">
+            <p>
+                <label class="checkbox">
+                <input type="checkbox" class="delete-tweets-checkbox" name="delete_tweets" {% if delete_tweets %}checked="checked"{% endif %} />
+                Delete old tweets
+                </label>
+            </p>
+            <fieldset class="delete-tweets-fieldset">
+                <legend>Tweets</legend>
+                <p>
+                    Delete tweets older than
+                    <input class="small" type="number" min="0"  name="tweets_days_threshold" value="{{ tweets_days_threshold }}" \>
+                    days
+                </p>
+                <p>
+                    Unless they have at least
+                    <input class="small" type="number" min="0"  name="tweets_retweet_threshold" value="{{ tweets_retweet_threshold }}" \>
+                    retweets
+                </p>
+                <p>
+                    Or at least
+                    <input class="small" type="number" min="0"  name="tweets_like_threshold" value="{{ tweets_like_threshold }}" \>
+                    likes
+                </p>
+                <p>
+                    <label class="checkbox">
+                        <input type="checkbox" name="tweets_threads_threshold" {% if tweets_threads_threshold %}checked="checked"{% endif %} />
+                        Don't delete tweets that are part of a thread that contains at least one tweet that meets these thresholds
+                    </label>
+                </p>
+            </fieldset>
+
+            <p>
+                <label class="checkbox">
+                <input type="checkbox" class="retweets-likes-checkbox" name="retweets_likes" {% if retweets_likes %}checked="checked"{% endif %} />
+                Unretweet and unlike old tweets
+                </label>
+            </p>
+
+            <fieldset class="retweets-likes-fieldset">
+                <legend>Retweets and likes</legend>
+
+                <p>
+                    <label class="checkbox">
+                        <input type="checkbox" name="retweets_likes_delete_retweets" {% if retweets_likes_delete_retweets %}checked="checked"{% endif %} />
+                        Unretweet tweets
+                    </label>
+                    older than
+                    <input class="small" type="number" min="0"  name="retweets_likes_retweets_threshold" value="{{ retweets_likes_retweets_threshold }}" \>
+                    days
+                </p>
+
+                <p>
+                    <label class="checkbox">
+                        <input type="checkbox" name="retweets_likes_delete_likes" {% if retweets_likes_delete_likes %}checked="checked"{% endif %} />
+                        Unlike tweets
+                    </label>
+                    older than
+                    <input class="small" type="number" min="0"  name="retweets_likes_likes_threshold" value="{{ retweets_likes_likes_threshold }}" \>
+                    days
+                </p>
+
+            </fieldset>
+
+            <p><input type="submit" value="Save" /></p>
+            </form>
         </div>
     `
 })
