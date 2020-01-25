1db7393c30a807945010ea6090971abacc9ca726
Micah Lee <micah@micahflee.com>
2020-01-24 17:38:10 -0800

    Working on dashboard

--- app/web/static/js/components/dashboard.js
+++ app/web/static/js/components/dashboard.js
@@ -1,7 +1,43 @@
+Vue.component('job', {
+    props: ["job"],
+    computed: {
+        scheduledTimestampInThePast: function () {
+            // job['scheduled_timestamp']
+        },
+        humanReadableScheduledTimestamp: function () {
+            // job['scheduled_timestamp']
+        }
+    },
+    template: `
+        <div v-bind:class="job.status">
+            <template v-if="job.type == 'fetch'">
+                <template v-if="job.status == 'pending'">
+                    <p v-if="scheduledTimestampInThePast">Waiting to download all of your tweets and likes as soon as it's your turn in the queue.</p>
+                    <p v-else>Waiting to download all of your tweets and likes, scheduled for <em>{{ humanReadableScheduledTimestamp }}</em>.</p>
+                </template>
+                <template v-else>
+                    <p>Downloading all of your tweets and likes. Progress: <span class="project">{{ job.progress }}</span></p>
+                </template>
+            </template>
+
+            <template v-if="job.type == 'delete'">
+                <template v-if="job.status == 'pending'">
+                    <p v-if="scheduledTimestampInThePast">Waiting to delete your old tweets and likes as soon as it's your turn in the queue.</p>
+                    <p v-else>Waiting to delete your old tweets and likes, scheduled for <em>{{ humanReadableScheduledTimestamp }}</em>.</p>
+                </template>
+                <template v-else>
+                    <p>Deleting your tweets and likes. Progress: <span class="project">{{ job.progress }}</span></p>
+                </template>
+            </template>
+        </div>
+    `
+})
+
 Vue.component('dashboard', {
     data: function () {
         return {
             loading: false,
+            paused: null,
             activeJobs: [],
             pendingJobs: []
         }
@@ -65,6 +101,8 @@ Vue.component('dashboard', {
                             that.pendingJobs = data['pending_jobs'];
                         else
                             that.pendingJobs = [];
+
+                        that.paused = data['paused'];
                     })
                 })
                 .catch(function (err) {
@@ -79,17 +117,44 @@ Vue.component('dashboard', {
 
             <p v-if="loading"><img src="/static/img/loading.gif" alt="Loading" /></p>
 
-            <div v-if="!loading && activeJobs.length == 0 && pendingJobs.length == 0">
-                Semiphemeral is <strong>paused</strong>. When you're sure that your
-                <button v-on:click="$emit('select-page', 'settings')">settings</button>
-                are exactly as you want them:
-                <button v-on:click="startSemiphemeral">Start Semiphemeral</button>
+            <div v-if="!loading && paused">
+                <p>Semiphemeral is <strong>paused</strong>. Before starting Semiphemeral:</p>
+                <ol>
+                    <li>
+                        Make sure your
+                        <button v-on:click="$emit('select-page', 'settings')">settings</button>
+                        are exactly as you want them
+                    </li>
+                    <li>
+                        Make sure you have manually chosen which of your old
+                        <button v-on:click="$emit('select-page', 'tweets')">tweets</button>
+                        you want to make sure don't get automatically deleted
+                    </li>
+                </ol>
+                <p v-if="pendingJobs.length == 0 and activeJobs.length == 0">
+                    <button v-on:click="startSemiphemeral">Start Semiphemeral</button>
+                </p>
+                <p v-else>
+                    <em>You must wait for Semiphemeral to finish download all your old tweets
+                    before you can start deleting, so you don't accidentally delete tweets
+                    you wished you had kept.</em>
+                </p>
             </div>
 
-            <div v-if="!loading && (activeJobs.length > 0 || pendingJobs.length > 0)">
-                Semiphemeral is <strong>active</strong>.
-                <button v-on:click="pauseSemiphemeral">Pause Semiphemeral</button>
+            <div v-if="!loading && !paused">
+                <p>
+                    Semiphemeral is <strong>active</strong>.
+                    <button v-on:click="pauseSemiphemeral">Pause Semiphemeral</button>
+                </p>
             </div>
+
+            <ul v-if="activeJobs.length > 0" v-for="job in activeJobs">
+                <job v-bind:job="job"></job>
+            </ul>
+
+            <ul v-if="pendingJobs.length > 0" v-for="job in pendingJobs">
+                <job v-bind:job="job"></job>
+            </ul>
         </div>
     `
 })
