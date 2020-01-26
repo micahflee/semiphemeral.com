e28cb86e69aade0fa71b1fdf9e82379f775c0be5
Micah Lee <micah@micahflee.com>
2020-01-25 22:19:38 -0800

    Add threads and tweets tables

--- app/web/twitter.py
+++ app/web/twitter.py
@@ -30,11 +30,11 @@ async def start_jobs():
     # jobs to "pending" so they'll start over
     await Job.update.values(status="pending").where(
         Job.status == "active"
-    ).gino().status()
+    ).gino.status()
 
     # Infinitely loop looking for pending jobs
     while True:
-        asyncio.sleep(60)
+        await asyncio.sleep(60)
 
         jobs = (
             await Job.query.where(Job.status == "pending")
