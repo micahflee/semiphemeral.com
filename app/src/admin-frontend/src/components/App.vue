f8a85f282d7bd26f6eeb5e333fe920946a6a09a7
Micah Lee <micah@micahflee.com>
2020-02-23 12:09:05 -0800

    Add admin console

---
+++ app/src/admin-frontend/src/components/App.vue
@@ -0,0 +1,119 @@
+<style>
+html {
+  position: relative;
+  min-height: 100%;
+}
+
+body {
+  margin: 0 0 25px;
+  padding: 10px;
+  font-family: sans;
+}
+
+p {
+  line-height: 150%;
+}
+
+li {
+  line-height: 150%;
+}
+
+#app {
+  max-width: 1000px;
+  margin: 0 auto;
+  font-size: 0.9em;
+}
+
+h1 {
+  font-size: 1.3em;
+}
+
+h2 {
+  font-size: 1.1em;
+}
+
+img.refresh {
+  margin-left: 1em;
+  height: 15px;
+  cursor: pointer;
+}
+
+a:link,
+a:visited {
+  color: #28404f;
+  text-decoration: underline;
+}
+
+a:active,
+a:hover {
+  color: #5d8fad;
+  text-decoration: none;
+}
+
+footer {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 25px;
+  width: 100%;
+  overflow: hidden;
+  text-align: right;
+  font-size: 0.7em;
+}
+
+footer p {
+  margin: 0 10px;
+}
+</style>
+
+<template>
+  <div>
+    <NavBar
+      v-on:select-page="selectPage"
+      v-bind="{
+            userScreenName: userScreenName,
+            userProfileUrl: userProfileUrl }"
+    ></NavBar>
+    <router-view v-bind="{
+      userScreenName: userScreenName
+    }"></router-view>
+  </div>
+</template>
+
+<script>
+import NavBar from "./layout/NavBar.vue";
+
+export default {
+  data: function() {
+    return {
+      userScreenName: false,
+      userProfileUrl: false
+    };
+  },
+  created: function() {
+    this.getUser();
+  },
+  methods: {
+    getUser: function() {
+      var that = this;
+      fetch("/api/user")
+        .then(function(response) {
+          if (response.status !== 200) {
+            console.log("Error fetching user, status code: " + response.status);
+            return;
+          }
+          response.json().then(function(data) {
+            that.userScreenName = data["user_screen_name"];
+            that.userProfileUrl = data["user_profile_url"];
+          });
+        })
+        .catch(function(err) {
+          console.log("Error fetching user", err);
+        });
+    }
+  },
+  components: {
+    NavBar: NavBar
+  }
+};
+</script>
---
+++ app/src/admin-frontend/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+    mode: process.env.DEPLOY_ENVIRONMENT == "production" ? "production" : "development",
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, '../static'),
+        filename: 'bundle-admin.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
