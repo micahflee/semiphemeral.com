be94fddaa75f0cdea27529a230f6405f9daf728d
Micah Lee <micah@micahflee.com>
2020-12-21 19:47:06 -0800

    Comment out reminder DMs

--- app/src/jobs.py
+++ app/src/jobs.py
@@ -1336,65 +1336,65 @@ async def start_dm_jobs():
         print(f"Waiting 1 minute")
         await asyncio.sleep(60)
 
-        # Only run this once a day
-        if minutes == 0:
-            # Do we need to send reminders?
-            print("Checking if we need to send reminders")
+        # # Only run this once a day
+        # if minutes == 0:
+        #     # Do we need to send reminders?
+        #     print("Checking if we need to send reminders")
 
-            message = f"Hello! Just in case you forgot about me, your Semiphemeral account has been paused for several months. You can login at https://{os.environ.get('DOMAIN')}/ to unpause your account and start automatically deleting your old tweets and likes, except for the ones you want to keep."
+        #     message = f"Hello! Just in case you forgot about me, your Semiphemeral account has been paused for several months. You can login at https://{os.environ.get('DOMAIN')}/ to unpause your account and start automatically deleting your old tweets and likes, except for the ones you want to keep."
 
-            three_months_ago = datetime.now() - timedelta(days=90)
-            reminded_users = []
+        #     three_months_ago = datetime.now() - timedelta(days=90)
+        #     reminded_users = []
 
-            # Find all the paused users
-            users = (
-                await User.query.where(User.blocked == False)
-                .where(User.paused == True)
-                .gino.all()
-            )
-            for user in users:
-                # Get the last job they finished
-                last_job = (
-                    await Job.query.where(Job.user_id == user.id)
-                    .where(Job.status == "finished")
-                    .order_by(Job.finished_timestamp.desc())
-                    .gino.first()
-                )
-                if last_job:
-                    # Was it it more than 3 months ago?
-                    if last_job.finished_timestamp < three_months_ago:
-                        remind = False
+        #     # Find all the paused users
+        #     users = (
+        #         await User.query.where(User.blocked == False)
+        #         .where(User.paused == True)
+        #         .gino.all()
+        #     )
+        #     for user in users:
+        #         # Get the last job they finished
+        #         last_job = (
+        #             await Job.query.where(Job.user_id == user.id)
+        #             .where(Job.status == "finished")
+        #             .order_by(Job.finished_timestamp.desc())
+        #             .gino.first()
+        #         )
+        #         if last_job:
+        #             # Was it it more than 3 months ago?
+        #             if last_job.finished_timestamp < three_months_ago:
+        #                 remind = False
 
-                        # Let's make sure we also haven't sent them a DM in the last 3 months
-                        last_dm_job = (
-                            await DirectMessageJob.query.where(
-                                DirectMessageJob.dest_twitter_id == user.twitter_id
-                            )
-                            .order_by(DirectMessageJob.sent_timestamp.desc())
-                            .gino.first()
-                        )
-                        if last_dm_job:
-                            if last_dm_job.scheduled_timestamp < three_months_ago:
-                                remind = True
-                        else:
-                            remind = True
+        #                 # Let's make sure we also haven't sent them a DM in the last 3 months
+        #                 last_dm_job = (
+        #                     await DirectMessageJob.query.where(
+        #                         DirectMessageJob.dest_twitter_id == user.twitter_id
+        #                     )
+        #                     .order_by(DirectMessageJob.sent_timestamp.desc())
+        #                     .gino.first()
+        #                 )
+        #                 if last_dm_job:
+        #                     if last_dm_job.scheduled_timestamp < three_months_ago:
+        #                         remind = True
+        #                 else:
+        #                     remind = True
 
-                        if remind:
-                            reminded_users.append(user.twitter_screen_name)
-                            await DirectMessageJob.create(
-                                dest_twitter_id=user.twitter_id,
-                                message=message,
-                                status="pending",
-                                scheduled_timestamp=datetime.now(),
-                            )
+        #                 if remind:
+        #                     reminded_users.append(user.twitter_screen_name)
+        #                     await DirectMessageJob.create(
+        #                         dest_twitter_id=user.twitter_id,
+        #                         message=message,
+        #                         status="pending",
+        #                         scheduled_timestamp=datetime.now(),
+        #                     )
 
-            if len(reminded_users) > 0:
-                admin_message = (
-                    f"Sent semiphemeral reminders to {len(reminded_users)} users:\n\n"
-                    + "\n".join(reminded_users)
-                )
-                await send_admin_dm(admin_message)
+        #     if len(reminded_users) > 0:
+        #         admin_message = (
+        #             f"Sent semiphemeral reminders to {len(reminded_users)} users:\n\n"
+        #             + "\n".join(reminded_users)
+        #         )
+        #         await send_admin_dm(admin_message)
 
-            minutes += 1
-            if minutes == 1440:
-                minutes = 0
+        #     minutes += 1
+        #     if minutes == 1440:
+        #         minutes = 0
