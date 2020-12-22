e934fbc5c7c63cdbd7e29e782887c26d32adda50
Micah Lee <micah@micahflee.com>
2020-12-21 19:03:12 -0800

    View active and pending jobs in admin

--- app/src/admin-frontend/src/components/pages/Jobs.vue
+++ app/src/admin-frontend/src/components/pages/Jobs.vue
@@ -4,21 +4,45 @@ ul {
   padding: 0;
 }
 
+li .job-id {
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 10px;
+  width: 50px;
+  font-size: 0.9em;
+}
+
 li .job-user {
   display: inline-block;
   vertical-align: middle;
-  margin-right: 0.5em;
+  margin-right: 10px;
   width: 150px;
   font-size: 0.9em;
+}
+
+li .job-type {
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 10px;
+  width: 60px;
+  font-size: 0.8em;
 }
 
 li .job-date {
   display: inline-block;
   vertical-align: middle;
-  margin-right: 0.5em;
-  width: 160px;
+  margin-right: 10px;
+  width: 250px;
   font-size: 0.8em;
   color: #666666;
+}
+
+li .job-progress {
+  display: block;
+  font-size: 0.9em;
+  font-family: monospace;
+  color: #333;
+  margin-left: 65px;
 }
 </style>
 
@@ -30,22 +54,20 @@ li .job-date {
       <h2>{{ active_jobs.length }} active jobs</h2>
       <ul>
         <li v-for="job in active_jobs">
-          <div v-if="job.twitter_username != None">
-            <span class="job-user">
-              <a v-bind:href="job.twitter_link" target="_blank">{{
-                job.twitter_username
-              }}</a>
-            </span>
-          </div>
-          <div v-else>
+          <span class="job-id">{{ job.id }}</span>
+          <span class="job-user" v-if="job.twitter_username != None">
+            <a v-bind:href="job.twitter_link" target="_blank">{{
+              job.twitter_username
+            }}</a>
+          </span>
+          <span class="job-user" v-else>
             <p>unknown user</p>
-          </div>
-
+          </span>
           <span class="job-type">{{ job.job_type }}</span>
-          <span class="job-progress">{{ job.progress }}</span>
           <span class="job-date"
             >started {{ formatJobDate(job.started_timestamp) }}</span
           >
+          <span class="job-progress">{{ job.progress }}</span>
         </li>
       </ul>
     </div>
@@ -54,6 +76,7 @@ li .job-date {
       <h2>{{ pending_jobs.length }} pending jobs</h2>
       <ul>
         <li v-for="job in pending_jobs">
+          <span class="job-id">{{ job.id }}</span>
           <div v-if="job.twitter_username != None">
             <span class="job-user">
               <a v-bind:href="job.twitter_link" target="_blank">{{
@@ -64,7 +87,6 @@ li .job-date {
           <div v-else>
             <p>unknown user</p>
           </div>
-
           <span class="job-type">{{ job.job_type }}</span>
           <span class="job-date"
             >scheduled {{ formatJobDate(job.scheduled_timestamp) }}</span
@@ -103,8 +125,8 @@ export default {
           }
           response.json().then(function (data) {
             that.loading = false;
-            if (data["active_jobs"]) that.active_jobs = data["active_jobs"];
-            if (data["pending_jobs"]) that.active_jobs = data["pending_jobs"];
+            that.active_jobs = data["active_jobs"];
+            that.pending_jobs = data["pending_jobs"];
           });
         })
         .catch(function (err) {
@@ -141,7 +163,17 @@ export default {
       } else if (month_num == 12) {
         month = "December";
       }
-      return month + " " + date.getDate() + ", " + date.getFullYear();
+      return (
+        month +
+        " " +
+        date.getDate() +
+        ", " +
+        date.getFullYear() +
+        " " +
+        date.getHours() +
+        ":" +
+        date.getMinutes()
+      );
     },
   },
 };
