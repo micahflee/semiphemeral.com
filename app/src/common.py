7a4d6734cdfd024e888aa05750c2296f941f4266
Micah Lee <micah@micahflee.com>
2021-04-01 09:51:18 -0700

    Handle more exceptions

--- app/src/common.py
+++ app/src/common.py
@@ -69,12 +69,14 @@ class PoenyErrorHandler(peony.ErrorHandler):
         return peony.ErrorHandler.RETRY
 
     @peony.ErrorHandler.handle(
-        peony.exceptions.InternalError, peony.exceptions.HTTPServiceUnavailable
+        peony.exceptions.InternalError,
+        peony.exceptions.HTTPServiceUnavailable,
+        peony.exceptions.OverCapacity,
     )
-    async def handle_delay_errors(self):
+    async def handle_delay_errors(self, exception):
         exception_str = str(exception).replace("\n", ", ")
-        await log(self.job, f"#{self.job_runner_id} {exception_str}, retrying in 30s")
-        await asyncio.sleep(30)
+        await log(self.job, f"#{self.job_runner_id} {exception_str}, retrying in 60s")
+        await asyncio.sleep(60)
         return peony.ErrorHandler.RETRY
 
     @peony.ErrorHandler.handle(Exception)
@@ -124,11 +126,16 @@ async def tweepy_api_call(api, method, **kwargs):
     https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.loop.run_in_executor
     https://docs.python.org/3/library/asyncio-eventloop.html#asyncio-pass-keywords
     """
-    loop = asyncio.get_running_loop()
-    result = await loop.run_in_executor(
-        None, functools.partial(getattr(api, method), **kwargs)
-    )
-    return result
+    while True:
+        try:
+            loop = asyncio.get_running_loop()
+            result = await loop.run_in_executor(
+                None, functools.partial(getattr(api, method), **kwargs)
+            )
+            return result
+        except tweepy.error.TweepError as e:
+            print(f"tweepy_api_call, hit exception, retrying in 60s: {e}")
+            await asyncio.sleep(60)
 
 
 async def tweepy_api(user):
