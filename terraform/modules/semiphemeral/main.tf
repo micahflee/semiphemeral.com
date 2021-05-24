f2c93bb831a4be68df19ed5eab8c9dd207e7b058
Micah Lee <micah@micahflee.com>
2021-05-23 20:14:32 -0700

    Run postgres in docker

--- terraform/modules/semiphemeral/main.tf
+++ terraform/modules/semiphemeral/main.tf
@@ -105,7 +105,7 @@ resource "digitalocean_droplet" "db" {
 
 resource "digitalocean_volume" "db_data" {
   region                  = "nyc1"
-  name                    = "db"
+  name                    = "db-${var.deploy_environment}"
   size                    = 100
   initial_filesystem_type = "ext4"
 }
