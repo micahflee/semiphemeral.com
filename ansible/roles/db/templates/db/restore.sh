651c5ce6684649e4df8b362ce6ff7d95e9a45ee1
Micah Lee <micah@micahflee.com>
2021-05-24 20:17:46 -0700

    Fix backup and restore scripts

--- ansible/roles/db/templates/db/restore.sh
+++ ansible/roles/db/templates/db/restore.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-FILENAME=/db/$1
+GZ_FILENAME=/db/$1
+FILENAME=$(echo $GZ_FILENAME | cut -d"." -f1-2)
 
-echo "== Restore"
+echo "== Decompressing" &&
+gunzip "$GZ_FILENAME" &&
+
+echo "== Restore" &&
 psql \
     -h "{{ db_private_ip }}" \
     -p 5432 \
     -U "{{ postgres_user }}" \
     -d "{{ postgres_db }}" \
-    -f $FILENAME
+    -f "$FILENAME" &&
 
-echo "== Database restored, deleting backup from server"
-rm $FILENAME
+echo "== Database restored, deleting backup from server" && 
+rm *.gz
