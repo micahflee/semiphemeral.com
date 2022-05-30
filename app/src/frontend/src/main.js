03d0ad9f9e8e0740483b923855576b504d11dd97
Micah Lee <micah@micahflee.com>
2022-05-29 19:54:38 -0700

    Rearrange files in frontend, and update main.js

--- app/src/frontend/src/main.js
+++ app/src/frontend/src/main.js
@@ -1,36 +1,34 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-import App from './components/App.vue';
-import Dashboard from "./components/pages/Dashboard.vue";
-import Tweets from "./components/pages/Tweets.vue";
-import Export from "./components/pages/Export.vue";
-import DirectMessages from "./components/pages/DirectMessages.vue";
-import Settings from "./components/pages/Settings.vue";
-import Tip from "./components/pages/Tip.vue";
-import Thanks from "./components/pages/Thanks.vue";
-import CancelTip from "./components/pages/CancelTip.vue";
-import Faq from "./components/pages/Faq.vue";
+import App from './App.vue';
+import Dashboard from "./pages/Dashboard.vue";
+import Tweets from "./pages/Tweets.vue";
+import Export from "./pages/Export.vue";
+import DirectMessages from "./pages/DirectMessages.vue";
+import Settings from "./pages/Settings.vue";
+import Tip from "./pages/Tip.vue";
+import Thanks from "./pages/Thanks.vue";
+import CancelTip from "./pages/CancelTip.vue";
+import Faq from "./pages/Faq.vue";
 
-Vue.use(VueRouter);
+const routes = [
+    { path: '/dashboard', name: 'dashboard', component: Dashboard },
+    { path: '/tweets', name: 'tweets', component: Tweets },
+    { path: '/export', name: 'export', component: Export },
+    { path: '/dms', name: 'dms', component: DirectMessages },
+    { path: '/settings', name: 'settings', component: Settings },
+    { path: '/tip', name: 'tip', component: Tip },
+    { path: '/thanks', name: 'thanks', component: Thanks },
+    { path: '/cancel-tip', name: 'cancel-tip', component: CancelTip },
+    { path: '/faq', name: 'faq', component: Faq }
+]
 
-const router = new VueRouter({
-    mode: 'history',
-    routes: [
-        { path: '/dashboard', name: 'dashboard', component: Dashboard },
-        { path: '/tweets', name: 'tweets', component: Tweets },
-        { path: '/export', name: 'export', component: Export },
-        { path: '/dms', name: 'dms', component: DirectMessages },
-        { path: '/settings', name: 'settings', component: Settings },
-        { path: '/tip', name: 'tip', component: Tip },
-        { path: '/thanks', name: 'thanks', component: Thanks },
-        { path: '/cancel-tip', name: 'cancel-tip', component: CancelTip },
-        { path: '/faq', name: 'faq', component: Faq }
-    ]
-});
-
-new Vue({
-    el: '#app',
-    render: h => h(App),
-    router
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
 })
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
