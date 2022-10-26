a3c92e1857b33ad51a4dc005b151c9f7b850e762
Micah Lee <micah@micahflee.com>
2022-10-26 15:42:42 -0700

    Temporarily change to 20 minutes

--- devops/delete-old-snapshots.py
+++ devops/delete-old-snapshots.py
@@ -15,7 +15,7 @@ def main():
         created_at = datetime.strptime(snapshot["created_at"], "%Y-%m-%dT%H:%M:%SZ")
         age = now - created_at
         if snapshot["name"].startswith("db-production-") and age >= timedelta(
-            hours=1
+            minutes=20
         ):
             print(f"Deleting snapshot: {snapshot['name']}, age: {age}")
             # subprocess.run(["doctl", "compute", "snapshot", "delete", "--force", snapshot["id"]])
