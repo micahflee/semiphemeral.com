0b38ed7df13eeff123c32feede1cf4e8715d775e
Micah Lee <micah@micahflee.com>
2022-10-26 15:42:15 -0700

    Age should be >=, not <=

--- devops/delete-old-snapshots.py
+++ devops/delete-old-snapshots.py
@@ -14,7 +14,7 @@ def main():
         now = datetime.utcnow()
         created_at = datetime.strptime(snapshot["created_at"], "%Y-%m-%dT%H:%M:%SZ")
         age = now - created_at
-        if snapshot["name"].startswith("db-production-") and age <= timedelta(
+        if snapshot["name"].startswith("db-production-") and age >= timedelta(
             hours=1
         ):
             print(f"Deleting snapshot: {snapshot['name']}, age: {age}")
