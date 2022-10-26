2fff7ae6e6b6c7a4575f948f5af42f07e3b5afe0
Micah Lee <micah@micahflee.com>
2022-10-26 15:30:09 -0700

    Add GitHub Actions workflow to save snapshots, and delete old snapshots

---
+++ devops/delete-old-snapshots.py
@@ -0,0 +1,27 @@
+import subprocess
+import json
+from datetime import datetime, timedelta
+
+
+def main():
+    snapshots = json.loads(
+        subprocess.check_output(
+            ["doctl", "compute", "snapshot", "list", "--output", "json"]
+        )
+    )
+
+    for snapshot in snapshots:
+        now = datetime.utcnow()
+        created_at = datetime.strptime(snapshot["created_at"], "%Y-%m-%dT%H:%M:%SZ")
+        age = now - created_at
+        if snapshot["name"].startswith("db-production-") and age <= datetime.timedelta(
+            hours=1
+        ):
+            print(f"Deleting snapshot: {snapshot['name']}")
+            print(subprocess.check_output(
+                ["doctl", "compute", "snapshot", "delete", snapshot["id"]]
+            ))
+
+
+if __name__ == "__main__":
+    main()
