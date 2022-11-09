4ebeab90301bb5414db26daa30f3aa7165698d5c
Micah Lee <micah@micahflee.com>
2022-11-08 16:05:50 -0800

    Make dms_worker wait for monitor too

--- app/src/worker.py
+++ app/src/worker.py
@@ -2,26 +2,38 @@ import socket
 import time
 import subprocess
 import sys
+import click
 
-# Wait for monitor port to be open
-while True:
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect(("monitor", 9181))
-        break
-    except:
-        print("Waiting for monitor to finish initializing...", file=sys.stderr)
-        time.sleep(30)
 
-# Start the worker
-print("Starting worker", file=sys.stderr)
-subprocess.run(
-    [
+@click.command()
+@click.option("--dms", is_flag=True, default=False)
+def main(dms):
+    # Wait for monitor port to be open
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect(("monitor", 9181))
+            break
+        except:
+            print("Waiting for monitor to finish initializing...", file=sys.stderr)
+            time.sleep(30)
+
+    # Start the worker
+    print("Starting worker", file=sys.stderr)
+    args = [
         "rq",
         "worker",
         "--url",
         "redis://redis:6379",
         "--with-scheduler",
-        "jobs",
     ]
-)
+    if dms:
+        args.append("dm_jobs_high")
+        args.append("dm_jobs_low")
+    else:
+        args.append("jobs")
+    subprocess.run(args)
+
+
+if __name__ == "__main__":
+    pass
