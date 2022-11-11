9f2cbbc8a227c5196de019183f19fa096614e4d0
Micah Lee <micah@micahflee.com>
2022-11-11 13:21:27 -0800

    Fix showing duration on admin jobs

--- app/src/admin-frontend/src/pages/Jobs.vue
+++ app/src/admin-frontend/src/pages/Jobs.vue
@@ -108,7 +108,7 @@ fetchJobs()
             </span>
             <span class="job-user" v-else></span>
             <span class="job-type">{{ job.job_type }}</span>
-            <span class="job-date">duration {{ duration }}</span>
+            <span class="job-date">duration {{ job.duration }}</span>
             <span class="job-data">{{ job.data }}</span>
           </li>
         </ul>
